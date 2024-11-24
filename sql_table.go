71762d2006925f80b6b2234949ed4212b744cf62
Boris Mustakimov <bhmustakimov@yandex.ru>
2024-11-24 22:58:31 +0500

    2

---
+++ sql_table.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+func sql_table() {
+	db, _ := sql.Open("sqlite", "./scheduler.db")
+
+	statement, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS scheduler (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		title TEXT NOT NULL,
+		comment TEXT,
+		repeat TEXT CHECK(length(repeat) <= 128)
+	);
+	CREATE INDEX IF NOT EXISTS idx_scheduler_date ON scheduler(date);
+`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	statement.Exec()
+
+}
